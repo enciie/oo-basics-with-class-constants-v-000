62ce6e05a3814fd4a9f1d7fa9a66d8a629fe1817
Nancy Jo <enciie@gmail.com>
2018-05-07 21:02:01 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,11 +7,14 @@ class Book
   def initialize(title)
     @title = title
   end
-  
-  def genre
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
+  end
+  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre 
   end
 
 end
